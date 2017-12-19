a14fe84672d56f290cf30dfbab20819ae6afd195
vianney <vianney@quasardb.net>
2017-12-19 16:08:26 +0100

    Case 1773 - Create a package regrouping api, server, utils and http.

--- README.md
+++ README.md
@@ -25,3 +25,8 @@ To compile the quasardb packages, you need to has these installed:
     cd qdb-server
     wget https://download.quasardb.net/quasardb/2.0/2.0.0rc7/server/qdb-2.0.0-linux-64bit-server.tar.gz
     ./pack.sh qdb-2.0.0-linux-64bit-server.tar.gz
+
+# Warning
+If you experience a problem while installing or upgrading after previously uninstalling an older version you can need to check the rights for the following files and folders. They should be `qdb:qdb`.
+- /etc/qdb/qdb_http_private.key
+- /var/log/qdb_http
--- test.sh
+++ test.sh
@@ -9,6 +9,7 @@ CONTAINER_ROOTFS=/var/lib/lxc/$CONTAINER_NAME/rootfs
 
 TEST_SUITE="$DISTRIBUTION.$RELEASE"
 
+QDB_ALL=$(ls qdb-all/*.rpm)
 QDB_API=$(ls qdb-api/*.rpm)
 QDB_SERVER=$(ls qdb-server/*.rpm)
 QDB_UTILS=$(ls qdb-utils/*.rpm)
@@ -97,6 +98,56 @@ echo "##teamcity[testFinished name='utils.uninstall']"
 echo "##teamcity[testStarted name='web-bridge.uninstall' captureStandardOutput='true']"
 sudo lxc-attach --clear-env -n $CONTAINER_NAME -- rpm -e qdb-web-bridge || echo "##teamcity[testFailed name='web-bridge.uninstall' message='Failed to uninstall web-bridge']"
 echo "##teamcity[testFinished name='web-bridge.uninstall']"
+
+
+# [ qdb-all tests] Need to be done separately to avoid conflicts
+echo "##teamcity[testStarted name='all.install' captureStandardOutput='true']"
+sudo lxc-attach --clear-env -n $CONTAINER_NAME -- rpm -i /mnt/$QDB_ALL || echo "##teamcity[testFailed name='all.install' message='Failed to install God package']"
+echo "##teamcity[testFinished name='all.install']"
+
+echo "Wait for qdbd to start: $DELAY seconds..."
+sleep $DELAY
+
+echo "##teamcity[testStarted name='all.qdbsh.put' captureStandardOutput='true']"
+sudo lxc-attach --clear-env -n $CONTAINER_NAME -- qdbsh --user-credentials-file=/etc/qdb/qdbsh_private.key --cluster-public-key=/var/share/qdb/cluster_public.key -c "blob_put alias content" || echo "##teamcity[testFailed name='all.qdbsh.put' message='Failed to put blob']"
+echo "##teamcity[testFinished name='all.qdbsh.put']"
+
+echo "##teamcity[testStarted name='all.qdbsh.get' captureStandardOutput='true']"
+RESULT=$(sudo lxc-attach --clear-env -n $CONTAINER_NAME -- qdbsh --user-credentials-file=/etc/qdb/qdbsh_private.key --cluster-public-key=/var/share/qdb/cluster_public.key -c "blob_get alias") || echo "##teamcity[testFailed name='all.qdbsh.get' message='Failed to get blob']"
+[ "$RESULT" = "content" ] || echo "##teamcity[testFailed name='all.qdbsh.get' message='Invalid output from blob_get']"
+echo "##teamcity[testFinished name='all.qdbsh.get']"
+
+echo "##teamcity[testStarted name='all.web-bridge.wget' captureStandardOutput='true']"
+sudo lxc-attach --clear-env -n $CONTAINER_NAME -- wget -qS http://127.0.0.1:8080 2>&1 || echo "##teamcity[testFailed name='all.web-bridge.wget' message='Failed to wget 127.0.0.1:8080']"
+echo "##teamcity[testFinished name='all.web-bridge.wget']"
+
+echo "##teamcity[testStarted name='reboot' captureStandardOutput='true']"
+echo "Stop container..."
+sudo lxc-stop -n $CONTAINER_NAME
+sudo lxc-wait -n $CONTAINER_NAME -s STOPPED
+echo "Start container..."
+sudo lxc-start -n $CONTAINER_NAME
+sudo lxc-wait -n $CONTAINER_NAME -s RUNNING
+echo "Wait $DELAY seconds..."
+sleep $DELAY
+echo "##teamcity[testFinished name='reboot']"
+
+echo "##teamcity[testStarted name='all.qdbsh.get.after-reboot' captureStandardOutput='true']"
+RESULT=$(sudo lxc-attach --clear-env -n $CONTAINER_NAME -- qdbsh --user-credentials-file=/etc/qdb/qdbsh_private.key --cluster-public-key=/var/share/qdb/cluster_public.key -c "blob_get alias") || echo "##teamcity[testFailed name='all.qdbsh.get.after-reboot' message='Failed to get blob']"
+[ "$RESULT" = "content" ] || echo "##teamcity[testFailed name='all.qdbsh.get' message='Invalid output from blob_get']"
+echo "##teamcity[testFinished name='all.qdbsh.get.after-reboot']"
+
+echo "##teamcity[testStarted name='all.web-bridge.wget.after-reboot' captureStandardOutput='true']"
+sudo lxc-attach --clear-env -n $CONTAINER_NAME -- wget -qS http://127.0.0.1:8080 2>&1 || echo "##teamcity[testFailed name='all.web-bridge.wget.after-reboot' message='Failed to wget 127.0.0.1:8080']"
+echo "##teamcity[testFinished name='all.web-bridge.wget.after-reboot']"
+
+
+echo "##teamcity[testStarted name='all.uninstall' captureStandardOutput='true']"
+sudo lxc-attach --clear-env -n $CONTAINER_NAME -- rpm -e qdb-all || echo "##teamcity[testFailed name='all.uninstall' message='Failed to uninstall God package']"
+echo "##teamcity[testFinished name='all.uninstall']"
+
+# [end of qdb-all tests]
+
 
 echo "##teamcity[testStarted name='stop' captureStandardOutput='true']"
 sudo lxc-destroy -f -n $CONTAINER_NAME
