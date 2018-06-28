2cbd9b4e16cc969ec35595efc59391b0e83972fa
vianney <vianney.plotton@gmail.com>
2018-06-28 16:15:29 +0200

    Case 2072 - Add qdb-railgun to utils package.

--- qdb-utils/pack.sh
+++ qdb-utils/pack.sh
@@ -2,6 +2,9 @@
 
 PACKAGE_TARBALL=$(readlink -e $1)
 BENCHMARK_TARBALL=$(readlink -e *qdb-benchmark-*.tar.gz)
+RAILGUN_TARBALL=$(readlink -e *qdb-railgun-*.tar.gz)
+
+echo "RAILGUN_TARBALL: $RAILGUN_TARBALL"
 
 cd $(dirname $0)
-../common/pack.sh qdb-utils.spec $PACKAGE_TARBALL $BENCHMARK_TARBALL
+../common/pack.sh qdb-utils.spec $PACKAGE_TARBALL $BENCHMARK_TARBALL $RAILGUN_TARBALL
