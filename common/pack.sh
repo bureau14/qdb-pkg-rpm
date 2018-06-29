933c424c9edf8e7315fd897261415d9a6d952fa1
vianney <vianney.plotton@gmail.com>
2018-06-29 16:52:25 +0200

    Remove unnecessary unpackaging.

--- common/pack.sh
+++ common/pack.sh
@@ -4,17 +4,11 @@ set -e
 
 SPEC_FILE=$1; shift
 PACKAGE_TARBALL=$1; shift
-if [[ "$1" == *"qdb-benchmark"* ]]; then
-    BENCHMARK_TARBALL=$1; shift
-fi
-if [[ "$1" == *"qdb-railgun"* ]]; then
-    RAILGUN_TARBALL=$1; shift
-fi
 
 PACKAGE_NAME=$(basename "$SPEC_FILE" '.spec')
 PACKAGE_VERSION=$(echo "$PACKAGE_TARBALL" | sed -r 's/.*qdb-(.*)-linux-.*/\1/')
 
-export PACKAGE_VERSION PACKAGE_NAME PACKAGE_TARBALL BENCHMARK_TARBALL RAILGUN_TARBALL
+export PACKAGE_VERSION PACKAGE_NAME PACKAGE_TARBALL
 envsubst < "$SPEC_FILE.in" > "$SPEC_FILE"
 
 mkdir -p "$HOME/rpmbuild/SOURCES"
--- qdb-utils/pack.sh
+++ qdb-utils/pack.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -eu
 
 PACKAGE_TARBALL=$(readlink -e $1)
-BENCHMARK_TARBALL=$(readlink -e *qdb-benchmark-*.tar.gz)
-RAILGUN_TARBALL=$(readlink -e *qdb-railgun-*.tar.gz)
-
-echo "RAILGUN_TARBALL: $RAILGUN_TARBALL"
 
 cd $(dirname $0)
-../common/pack.sh qdb-utils.spec $PACKAGE_TARBALL $BENCHMARK_TARBALL $RAILGUN_TARBALL
+../common/pack.sh qdb-utils.spec $PACKAGE_TARBALL
