b3e29e6efdbcf2c6a5168abe4950da9096c151c7
Leon Mergen <leon@solatis.com>
2018-09-04 14:52:36 +0200

    Unbreak qdb-server on RHEL, move to systemd

--- qdb-server/pack.sh
+++ qdb-server/pack.sh
@@ -3,4 +3,4 @@
 PACKAGE_TARBALL=$(readlink -e $1)
 
 cd $(dirname $0)
-../common/pack.sh qdb-server.spec $PACKAGE_TARBALL qdbd_init.sh qdbd_limits.conf qdbd_sysctl.conf
+../common/pack.sh qdb-server.spec $PACKAGE_TARBALL qdbd.service qdbd_limits.conf qdbd_sysctl.conf
