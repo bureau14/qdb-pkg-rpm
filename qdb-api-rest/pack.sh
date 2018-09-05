e24ef3afad25c6931ac4233f3483125935fa4eb8
Leon Mergen <leon@solatis.com>
2018-09-05 07:34:10 +0200

    QDB-2048 Moves from SysV to SystemD (#3)
    Since SysV has been deprecated in RHEL7, we move to SystemD so everything works out of the box again, and as a bonus we can share configuration with the debian scripts.

--- qdb-api-rest/pack.sh
+++ qdb-api-rest/pack.sh
@@ -3,4 +3,4 @@
 PACKAGE_TARBALL=$(readlink -e $1)
 
 cd $(dirname $0)
-../common/pack.sh qdb-api-rest.spec $PACKAGE_TARBALL qdb_api_rest_init.sh
+../common/pack.sh qdb_rest.spec $PACKAGE_TARBALL qdb_rest.service
