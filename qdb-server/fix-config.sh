5aa36800e2a269ad14ecfe319a4680c1eccd316c
Leon Mergen <leon@solatis.com>
2018-09-12 11:01:32 +0200

    We finally have docker/qdbd working

---
+++ qdb-server/fix-config.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+IP=`which ip`
+AWK=`which awk`
+
+HOSTNAME=$(hostname)
+
+IP=$(cat /etc/hosts | grep ${HOSTNAME} | ${AWK} '{print $1}')
+
+cat /etc/qdb/qdbd.conf \
+    | jq ".local.network.listen_on = \"${IP}:2836\"" \
+    | jq ".global.security.enabled = false" \
+    | jq "del(.global.security.cluster_private_file)" \
+    | jq "del(.global.security.user_list)" \
+    > /tmp/qdbd.conf.new && \
+    mv /tmp/qdbd.conf.new /etc/qdb/qdbd.conf
+
+chown qdb:qdb /etc/qdb/qdbd.conf
