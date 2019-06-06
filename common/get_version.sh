aeea46d1e98ab34ba3a23ae5e62c40a5abb2a267
Marek Kurdej <marek@quasardb.net>
2019-06-06 11:29:43 +0200

    For non-release versions, set tarball version to ${VERSION}master_${DATE} (e.g. 3.2.1master_20190101), don't keep branch name.

--- common/get_version.sh
+++ common/get_version.sh
@@ -2,11 +2,15 @@
 
 TARBALL=$1
 
-if [[ ${TARBALL} = *"master"* ]]
+# Check if there is only version without branch name (e.g. master or qdb-1234)
+if [[ ${TARBALL} =~ -[0-9.]+- ]]
 then
-    VERSION=$(echo ${TARBALL} | sed -r 's/.*qdb-([0-9\.a-z_]+)-linux-.*/\1/')
-    DATE=$(date +'%Y%m%d')
-    echo "${VERSION}_${DATE}"
+    # Release
+    VERSION=$(echo ${TARBALL} | sed -r 's/.*qdb-([0-9\.]+)-linux-.*/\1/')
+    echo ${VERSION}
 else
-    echo ${TARBALL} | sed -r 's/.*qdb-([0-9\.a-z_]+)-linux-.*/\1/'
+    # Dev
+    VERSION=$(echo ${TARBALL} | sed -r 's/.*qdb-([0-9\.]+)([0-9\.a-z_]+)-linux-.*/\1/')
+    DATE=$(date +'%Y%m%d')
+    echo "${VERSION}master_${DATE}"
 fi
