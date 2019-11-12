1ef4c162ff84d6b25dfb127a28307a0002f7b01e
vianney <vianney.p@outlook.com>
2019-11-12 21:46:39 +0100

    Add version parameter to qdb-api.

--- qdb-api/pack.sh
+++ qdb-api/pack.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -eu
 
 PACKAGE_TARBALL=$(readlink -e $1)
+shift;
+PACKAGE_VERSION=$1; shift
 
 cd $(dirname $0)
-../common/pack.sh qdb-api.spec $PACKAGE_TARBALL
+../common/pack.sh qdb-api.spec $PACKAGE_TARBALL $PACKAGE_VERSION
