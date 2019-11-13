37c55389e854c6e6c3d506fcf984a3ec0f96ee36
vianney <vianney.p@outlook.com>
2019-11-13 18:20:53 +0100

    Set pkg version from tc.

--- qdb-server/pack.sh
+++ qdb-server/pack.sh
@@ -1,6 +1,7 @@
-#!/bin/sh -eu
+#!/bin/bash -eu
 
-PACKAGE_TARBALL=$(readlink -e $1)
+PACKAGE_TARBALL=$(readlink -e $1); shift
+PACKAGE_VERSION=$1; shift
 
 cd $(dirname $0)
-../common/pack.sh qdb-server.spec $PACKAGE_TARBALL qdbd.service qdbd_limits.conf qdbd_sysctl.conf
+../common/pack.sh qdb-server.spec $PACKAGE_TARBALL $PACKAGE_VERSION qdbd.service qdbd_limits.conf qdbd_sysctl.conf
