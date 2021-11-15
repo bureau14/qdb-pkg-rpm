b271bc6dbf39e7b35509aa2790b4f7ba15bc175e
Leon Mergen <leon@solatis.com>
2021-11-15 13:16:18 +0100

    Cleanup, add logrotate to dependency

--- qdb-server/pack.sh
+++ qdb-server/pack.sh
@@ -3,4 +3,8 @@
 PACKAGE_TARBALL=$(readlink -e $1); shift
 
 cd $(dirname $0)
-../common/pack.sh qdb-server.spec $PACKAGE_TARBALL qdbd.service qdbd_limits.conf qdbd_sysctl.conf
+../common/pack.sh qdb-server.spec \
+                  $PACKAGE_TARBALL \
+                  qdbd.service \
+                  qdbd_sysctl.conf \
+                  qdbd_logrotate.conf
