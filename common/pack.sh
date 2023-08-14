e48dbf35ca1b99a2f234a80fe3890f33b2cbe45d
Leon Mergen <leon@solatis.com>
2023-08-14 19:41:02 +0700

    Enable multi-arch builds

--- common/pack.sh
+++ common/pack.sh
@@ -7,13 +7,20 @@ PACKAGE_TARBALL=$1; shift
 
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-PACKAGE_VERSION=$(${MYDIR}/get_version.sh)
+# PACKAGE_ARCH is x86_64 by default, but aarch64 if the tarball contains
+# that name.
+PACKAGE_ARCH="x86_64"
+if [[ $PACKAGE_TARBALL == *"aarch64"* ]]; then
+    PACKAGE_ARCH="aarch64"
+fi
 
+PACKAGE_VERSION=$(${MYDIR}/get_version.sh)
 PACKAGE_NAME=$(basename "$SPEC_FILE" '.spec')
 
 echo "PACKAGE_VERSION: $PACKAGE_VERSION"
 echo "PACKAGE_NAME: $PACKAGE_NAME"
 echo "PACKAGE_TARBALL: $PACKAGE_TARBALL"
+echo "PACKAGE_ARCH: $PACKAGE_ARCH"
 
 export PACKAGE_VERSION PACKAGE_NAME PACKAGE_TARBALL
 envsubst < "$SPEC_FILE.in" > "$SPEC_FILE"
