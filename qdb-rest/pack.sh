b571c6f2a7967555a9c3e703a69803969c3e47d6
Joaquin Palma Ugarte <jr63.palma@gmail.com>
2025-02-07 11:40:19 -0500

    QDB-15526 - Add logrotation to REST API (#6)

--- qdb-rest/pack.sh
+++ qdb-rest/pack.sh
@@ -3,4 +3,4 @@
 PACKAGE_TARBALL=$(readlink -e $1); shift
 
 cd $(dirname $0)
-../common/pack.sh qdb-rest.spec $PACKAGE_TARBALL qdb_rest.service
+../common/pack.sh qdb-rest.spec $PACKAGE_TARBALL qdb_rest.service qdb_rest_logrotate.conf
